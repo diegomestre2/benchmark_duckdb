aa7203cfc3c051e07f579f014e061398fea0d27a
Diego Gomes Tome <diegogomest@gmail.com>
2022-01-25 14:27:52 +0100

    update

---
+++ scripts/experiments.py
@@ -0,0 +1,41 @@
+import duckdb
+
+runs = 10
+probe_side = 1024
+build_side = 1024
+con = duckdb.connect(':memory:')
+query = "SELECT test1.r, test2.r FROM test1,test2 WHERE test1.r=test2.r;"
+
+
+def create_tables():
+    con.execute("CREATE TABLE test1 AS SELECT range r FROM range(0, 1024, 1);")
+    con.execute("CREATE TABLE test2(r integer);")
+
+
+def populate_data(probe_times_build):
+    for i in range(0, probe_times_build):
+        con.execute("INSERT INTO test2 SELECT * FROM test1;")
+
+
+def run_and_profile_query(query):
+    con.execute("PRAGMA enable_profiling;")
+    con.execute("PRAGMA profiling_output='out.log';")
+    con.execute("PRAGMA profiling_mode=detailed;")
+    con.execute(query)
+    con.execute(
+        "SELECT EXTRA_INFO, TIME FROM pragma_detailed_profiling_output();")
+    print(con.fetchall())
+
+
+def main():
+    create_tables()
+    for probe_range in [10, 100, 1000, 10000, 100000, 1000000]:
+        print("Probe size: " + str(probe_range*1000), end='')
+        populate_data(probe_range)
+        run_and_profile_query(query)
+
+
+if __name__ == "__main__":
+    main()
+
+'''Test duckdb when installed with pip install duckdb'''
---
+++ scripts/experiments_primary_key.py
@@ -0,0 +1,53 @@
+import os
+from os import path
+import shutil
+from pathlib import Path
+
+home_dir = str(Path.home())
+master_link = "https://github.com/duckdb/duckdb.git"
+fork_link = "https://github.com/diegomestre2/duckdb.git"
+master_dir = "master_duckdb"
+fork_dir = "fork_duckdb"
+tpch_queries = ["q01", "q02", "q03", "q04", "q05", "q06", "q07", "q08", "q09", "q10",
+                "q11", "q12", "q13", "q14", "q15", "q16", "q17", "q18", "q19", "q20", "q21", "q22"]
+
+
+def remove_python_duckdb():
+    os.system("pip uninstall duckdb")
+
+
+def remove_dir(dir_path):
+    if path.exists(dir_path):
+        shutil.rmtree(dir_path)
+
+
+def run_duckdb_tpch(dir_path="", queries=tpch_queries):
+    if not path.exists(dir_path):
+        current_dir = os.getcwd()
+        current_dir += "/duckdb"
+        if not path.exists(current_dir):
+            raise Exception("This path does not exist: " + dir_path)
+        dir_path = current_dir
+    # Build
+    print(dir_path)
+    os.system("cd " + dir_path + " && BUILD_BENCHMARK=1 BUILD_TPCH=1 make -j4")
+    # Execute each query individually
+    for query in queries:
+        os.system("cd " + dir_path + " && build/release/benchmark/benchmark_runner " +
+                  "benchmark/tpch/sf1/"+query+".benchmark")
+
+
+def clone_duckdb(link, dir_path="duckdb"):
+    os.system("git clone " + str(link) + " " + str(dir_path))
+
+
+def main():
+    test_queries = ["q09"]
+    clone_duckdb(link=master_link)
+    run_duckdb_tpch(queries=test_queries)
+    clone_duckdb(link=fork_link, dir_path=fork_dir)
+    run_duckdb_tpch(dir_path=fork_dir, queries=test_queries)
+
+
+if __name__ == "__main__":
+    main()
