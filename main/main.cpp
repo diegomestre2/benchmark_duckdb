305b18f1db4b157a3f049f63a6b6abf0c08edce7
Diego Gomes Tome <diegogomest@gmail.com>
2022-01-26 17:08:43 +0100

    Add some tests for tpch

--- main/main.cpp
+++ main/main.cpp
@@ -8,12 +8,25 @@
 #include <variant>
 using namespace std;
 
-int main(int argc, char **argv) {
-	DuckDB db(nullptr);
-	Connection con(db);
+void duckdb_hello(Connection &con) {
 
+	con.EnableProfiling();
 	con.Query("CREATE TABLE integers(i INTEGER)");
 	con.Query("INSERT INTO integers VALUES (3)");
 	auto result = con.Query("SELECT * FROM integers");
 	result->Print();
+}
+void create_tpch_without_pk(Connection &con) {
+	auto result = con.Query("CALL dbgen(sf=0.1);");
+	result->Print();
+	result = con.Query("PRAGMA show_tables;");
+	result = con.Query("PRAGMA table_info(lineitem);");
+	result->Print();
+}
+
+int main(int argc, char **argv) {
+	DuckDB db(nullptr);
+	Connection con(db);
+	duckdb_hello(con);
+	create_tpch_without_pk(con);
 }
