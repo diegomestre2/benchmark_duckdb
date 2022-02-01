fbd66b0087ba281887cf8e1a42db3796ebf39c74
Diego Gomes Tome <diegogomest@gmail.com>
2022-02-01 16:59:31 +0100

    Bushy tpch q09

--- main/main.cpp
+++ main/main.cpp
@@ -28,33 +28,3 @@ int main(int argc, char **argv) {
 	duckdb_hello(con);
 	create_tpch_without_pk(con);
 }
-
-SELECT
-    nation,
-    o_year,
-    sum(amount) AS sum_profit
-FROM (
-    SELECT
-        n_name AS nation,
-        extract(year FROM o_orderdate) AS o_year,
-        l_extendedprice * (1 - l_discount) - ps_supplycost * l_quantity AS amount
-    FROM (
-        SELECT ps_supplycost, ps_partkey, n_name 
-        FROM supplier 
-        JOIN lineitem ON s_suppkey = l_suppkey
-        JOIN partsupp ON ps_suppkey = s_suppkey
-        JOIN part ON ps_partkey = p_partkey
-        JOIN nation ON s_nationkey = n_nationkey
-        WHERE p_name LIKE '%green%') AS temp,
-        lineitem,
-        orders
-    WHERE
-        ps_suppkey = l_suppkey
-        AND ps_partkey = l_partkey
-        AND o_orderkey = l_orderkey
-GROUP BY
-    nation,
-    o_year
-ORDER BY
-    nation,
-    o_year DESC;
--- scripts/tpch_q09_bushy.sql
+++ scripts/tpch_q09_bushy.sql
@@ -1,29 +1,23 @@
- SELECT
-     nation,
-     o_year,
-     sum(amount) AS sum_profit
- FROM (
+SELECT
+    nation,
+    o_year,
+    sum(amount) AS sum_profit
+FROM (
         SELECT
             n_name AS nation,
             extract(year FROM o_orderdate) AS o_year,
-            l_extendedprice * (1 - l_discount) - temp.ps_supplycost * l_quantity AS amount
-        FROM (
-                SELECT ps_supplycost, 
-                    ps_partkey, n_name
-                FROM supplier
-                    JOIN lineitem ON s_suppkey = l_suppkey
-                    JOIN partsupp ON ps_suppkey = s_suppkey
-                    JOIN part ON ps_partkey = p_partkey
-                    JOIN nation ON s_nationkey = n_nationkey
-                WHERE p_name LIKE '%green%'
-            ) AS temp
-        JOIN lineitem ON  ps_partkey = l_partkey
-        JOIN orders ON o_orderkey = l_orderkey
-    ) as temp2
+            l_extendedprice * (1 - l_discount) - ps_supplycost * l_quantity AS amount
+        FROM
+            lineitem 
+            JOIN (SELECT p_partkey FROM part WHERE p_name LIKE '%green%') as temp ON p_partkey = l_partkey 
+            JOIN partsupp ON ps_partkey = l_partkey AND ps_suppkey = l_suppkey
+            JOIN orders ON o_orderkey = l_orderkey
+            JOIN supplier ON s_suppkey = l_suppkey
+            JOIN nation ON s_nationkey = n_nationkey
+ ) as profit
 GROUP BY
     nation,
     o_year
 ORDER BY
     nation,
     o_year DESC;
-
